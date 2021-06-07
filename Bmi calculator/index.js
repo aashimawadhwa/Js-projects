b42c9081f19456dc25bb66bf95aa25c650b72b4f
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-06-08 00:21:07 +0530

    love calculator added

--- Bmi calculator/index.js
+++ Bmi calculator/index.js
@@ -1,8 +1,9 @@
 
 function bmicalc(){
  var height = document.getElementById("floatingTextarea1").value; 
-  var weight = document.getElementById("floatingTextarea2"); 
+ var weight = document.getElementById("floatingTextarea2").value; 
  var bmicalc = weight /(height * height);
+ bmicalc = bmicalc.toFixed(2)
   
-    alert(bmicalc);
+    alert("your Bmi index is " + bmicalc);
 }
