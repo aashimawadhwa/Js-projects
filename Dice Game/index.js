425b5997d746438bc054305af7736851a714f092
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-06-11 00:58:36 +0530

    Added dice game

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -2,8 +2,10 @@ var randomnumber1 = Math.floor(Math.random() *6) +1;
 var randomDiceImage = "dice" + randomnumber1 + ".png"
 var randomImageSource = "images/" + randomDiceImage ;
 var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomImageSource1);
 
 var randomnumber2 = Math.floor(Math.random() *6) +1;
 var randomImageSource2 = "images/dice" + randomnumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
+
