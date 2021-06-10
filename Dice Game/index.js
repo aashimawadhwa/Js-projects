39edd2a051b0d646073e3628554d0ab7246c72ca
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-06-11 01:15:56 +0530

    Added dice game

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -9,3 +9,13 @@ var randomImageSource2 = "images/dice" + randomnumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
 
+
+    if(randomnumber1 > randomnumber2) {
+        document.querySelector ("h1").innerHTML ="Player 1 wins";
+    }
+    else if (randomnumber2 >randomnumber1) {
+        document.querySelector ("h1").innerHTML ="Player 2 wins";
+    }
+    else   {
+        document.querySelector ("h1").innerHTML ="Draw!!";
+    }
