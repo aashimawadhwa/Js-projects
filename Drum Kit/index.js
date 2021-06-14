8eebfcc39202d8b8b9c534235c59e60f66a1d693
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-06-14 23:16:23 +0530

    Added Drum Kit

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -4,6 +4,41 @@
 
     document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
 
-    alert("i got clicked!");
+    var buttonInnerHTML =this.innerHTML;
+    switch (buttonInnerHTML){
+       case "w" :
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+
+      case "a" :
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+      case "s" :
+               var tom3 = new Audio("sounds/tom-3.mp3");
+               tom3.play();
+               break;
+               
+      case "d" :
+                  var tom4 = new Audio("sounds/tom-4.mp3");
+                  tom4.play();
+                  break;
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    
     });
  }
