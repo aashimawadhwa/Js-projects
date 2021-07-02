4bb7e96af0d7a3cb284620af94fa2b54d79cf0c6
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-07-03 02:35:45 +0530

    added calculator(express.js)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express(); 
+app.use(express.urlencoded({extended: true}));
 
 app.get("/" , function(req, res){
       res.sendFile(__dirname+"/index.html");
 });
+app.post("/", function (req , res){
+    var num1 = req.body.num1;
+    var num2 = req.body.num2;
+
+    var
+})
 
 app.listen(3000 ,function(){
     console.log("server started on port 3000");
